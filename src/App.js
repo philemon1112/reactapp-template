b31ba395fd4ce8d7adf5415f7faa72114906a751
philemon <philemonabakahmensah@gmail.com>
2024-02-15 21:31:16 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black">
+      <h2 className="font-semibold text-2xl text-green-500 ">Tailwind is working</h2>
     </div>
   );
 }
